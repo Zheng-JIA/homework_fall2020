Correct discounted cumsum functions. Now the lunar lander continuous works and average return is 199

hw2/cs285/agents/pg_agent.py
@@ -84,6 +84,7 @@ class PGAgent(BaseAgent):
         # by querying the neural network that you're using to learn the baseline
         if self.nn_baseline:
             baselines_unnormalized = self.actor.run_baseline_prediction(obs)
+            #print(np.mean(baselines_unnormalized))
             ## ensure that the baseline and q_values have the same dimensionality
             ## to prevent silent broadcasting errors
             assert baselines_unnormalized.ndim == q_values.ndim
@@ -148,7 +149,17 @@ class PGAgent(BaseAgent):
             # because the summation happens over [t, T] instead of [0, T]
         # HINT2: it is possible to write a vectorized solution, but a solution
             # using a for loop is also fine
-        discounted_rewards = np.array([(self.gamma ** t) * rewards[t] for t in range(len(rewards))])
-        list_of_discounted_cumsums = discounted_rewards[::-1].cumsum()[::-1]
+
+        #discounted_rewards = np.array([(self.gamma ** t) * rewards[t] for t in range(len(rewards))])
+        #list_of_discounted_cumsums = discounted_rewards[::-1].cumsum()[::-1]
+        #print(rewards)
+        list_of_discounted_cumsums = []
+        T = len(rewards)
+        Gt = 0
+        for t in range(T)[::-1]:
+            Gt = self.gamma * Gt + rewards[t]
+            list_of_discounted_cumsums.append(Gt)
+        list_of_discounted_cumsums = list_of_discounted_cumsums[::-1]
+        #print(list_of_discounted_cumsums)
         return list_of_discounted_cumsums
 

hw2/cs285/policies/MLP_policy.py
@@ -145,10 +145,10 @@ class MLPPolicyPG(MLPPolicy):
         self.baseline_loss = nn.MSELoss()
 
     def update(self, observations, actions, advantages, q_values=None):
-        # dim of observations: (close to batch_size, ob_dim)
-        # dim of actions: (close to batch_size)
-        # dim of advantages:  (close to batch_size)
-        # dim of q_values: (close to batch_size)
+        # dim of observations: (batch_size, ob_dim)
+        # dim of actions: (batch_size)
+        # dim of advantages:  (batch_size)
+        # dim of q_values: (batch_size)
         observations = ptu.from_numpy(observations)
         actions = ptu.from_numpy(actions)
         advantages = ptu.from_numpy(advantages)
@@ -180,13 +180,17 @@ class MLPPolicyPG(MLPPolicy):
         if self.nn_baseline:
             ## TODO: normalize the q_values to have a mean of zero and a standard deviation of one
             ## HINT: there is a `normalize` function in `infrastructure.utils`
+            #print("Q values are")
+            #print(np.max(q_values))
+
             targets = utils.normalize(q_values, np.mean(q_values), np.std(q_values))
             targets = ptu.from_numpy(targets)
             #print(targets.shape)
             ## TODO: use the `forward` method of `self.baseline` to get baseline predictions
-            baseline_predictions = self.baseline(observations).squeeze()
+            baseline_predictions = self.baseline(observations)
+            #print(torch.std(baseline_predictions))
+            baseline_predictions = baseline_predictions.squeeze()
 
-            #print(baseline_predictions.squeeze())
             ## avoid any subtle broadcasting bugs that can arise when dealing with arrays of shape
             ## [ N ] versus shape [ N x 1 ]
             ## HINT: you can use `squeeze` on torch tensors to remove dimensions of size 1
@@ -194,8 +198,10 @@ class MLPPolicyPG(MLPPolicy):
             
             # TODO: compute the loss that should be optimized for training the baseline MLP (`self.baseline`)
             # HINT: use `F.mse_loss`
-            baseline_loss = self.baseline_loss(targets, baseline_predictions.squeeze())
-
+            baseline_loss = self.baseline_loss(targets, baseline_predictions)
+            #print(targets)
+            #print(baseline_predictions)
+            #print(baseline_loss)
             # TODO: optimize `baseline_loss` using `self.baseline_optimizer`
             # HINT: remember to `zero_grad` first
             self.baseline_optimizer.zero_grad()